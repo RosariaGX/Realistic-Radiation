6a52ba9adb1603c88c2191bcbf82adf808d5df7b
RosarianGX <rosegx8799@gmail.com>
2025-02-26 21:39:47 +0000

    Project Init Commit

---
+++ RosariaGX.RealisticRadiation/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Realistic_Radiation.RosariaGX.RealisticRadiation;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
